603dfd5bb85e72505cfec2b920f52f70165e1577
Alex <seremet.alexandru.work@gmail.com>
2023-08-25 11:16:00 +0300

    took some configs and plugins from theprimeagen

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,26 +62,8 @@ return packer.startup(function(use)
 	use({ "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" })
 	use("navarasu/onedark.nvim")
 
-	-- Cmp
-	use({ "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" }) -- The completion plugin
-	use({ "hrsh7th/cmp-buffer", commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" }) -- buffer completions
-	use({ "hrsh7th/cmp-path", commit = "447c87cdd6e6d6a1d2488b1d43108bfa217f56e1" }) -- path completions
-	use({ "saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" }) -- snippet completions
-	use({ "hrsh7th/cmp-nvim-lsp", commit = "3cf38d9c957e95c397b66f91967758b31be4abe6" })
-	use({ "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" })
-
-	-- Snippets
-	use({ "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" }) --snippet engine
-	use({ "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" }) -- a bunch of snippets to use
-
-	-- LSP
-	use({ "neovim/nvim-lspconfig", commit = "f11fdff7e8b5b415e5ef1837bdcdd37ea6764dda" }) -- enable LSP
-	use({ "williamboman/mason.nvim", commit = "c2002d7a6b5a72ba02388548cfaf420b864fbc12" }) -- simple to use language server installer
-	use({ "williamboman/mason-lspconfig.nvim", commit = "0051870dd728f4988110a1b2d47f4a4510213e31" })
-
-
 	use({ "jose-elias-alvarez/null-ls.nvim", commit = "c0c19f32b614b3921e17886c541c13a72748d450" }) -- for formatters and linters
-  use({"jay-babu/mason-null-ls.nvim"})
+	use({ "jay-babu/mason-null-ls.nvim" })
 	use({ "RRethy/vim-illuminate", commit = "d6ca7f77eeaf61b3e6ce9f0e5a978d606df44298" })
 
 	-- Telescope
@@ -157,8 +139,35 @@ return packer.startup(function(use)
 		end,
 
 		-- The Primeagen
-		use({ "ThePrimeagen/vim-be-good", }),
-    use({ "ThePrimeagen/harpoon", }),
+		use({ "ThePrimeagen/vim-be-good" }),
+		use({ "ThePrimeagen/harpoon" }),
+
+		use("mbbill/undotree"),
+		use("nvim-treesitter/nvim-treesitter-context"),
+		use({
+			"VonHeikemen/lsp-zero.nvim",
+			branch = "v1.x",
+			requires = {
+				-- LSP Support
+				{ "neovim/nvim-lspconfig" },
+				{ "williamboman/mason.nvim" },
+				{ "williamboman/mason-lspconfig.nvim" },
+
+				-- Autocompletion
+				{ "hrsh7th/nvim-cmp" },
+				{ "hrsh7th/cmp-buffer" },
+				{ "hrsh7th/cmp-path" },
+				{ "saadparwaiz1/cmp_luasnip" },
+				{ "hrsh7th/cmp-nvim-lsp" },
+				{ "hrsh7th/cmp-nvim-lua" },
+
+				-- Snippets
+				{ "L3MON4D3/LuaSnip" },
+				{ "rafamadriz/friendly-snippets" },
+			},
+		}),
+
+		use("folke/zen-mode.nvim"),
 	})
 
 	-- Automatically set up your configuration after cloning packer.nvim
